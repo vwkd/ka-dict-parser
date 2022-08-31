295ea9d49938a1a56a4b65d8ae7e8f2dd1468cea
vwkd <33468089+vwkd@users.noreply.github.com>
2022-08-31 11:45:17 +0400

    parser: start "hello world"

---
+++ src/mod.ts
@@ -0,0 +1,30 @@
+import {
+  str,
+  sequenceOf,
+  choice,
+  char
+} from './deps.ts';
+
+// ---------- Parser ----------
+
+const helloString = str("hello");
+
+const spaceChar = char(" ");
+
+const nameString = choice([
+  str("there"),
+  str("everyone"),
+  str("world"),
+]);
+
+const fullParser = sequenceOf([
+  helloString,
+  spaceChar,
+  nameString,
+]);
+
+// ---------- Execution ----------
+
+const input = "Hello world";
+
+const result = fullParser.run(input).result;
