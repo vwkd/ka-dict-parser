f3047776b787a92b64bb57c059ccd7ab0a24e197
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-01 01:38:53 +0400

    add more numbers to test string

--- src/mod.ts
+++ src/mod.ts
@@ -11,6 +11,8 @@ const input = `ადგილ umstellen, translozieren
 ბალთ {kach., psch.} zerstückeln
 ბორძიკ 1. stolpern 2. stammeln
 ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
+გებულ 1. verstehen 2. vernehmen 3. gewinnen
+გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben
 `;
 
 parser.fork(input,
