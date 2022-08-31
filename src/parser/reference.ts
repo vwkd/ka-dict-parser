3a7367c4027c569b5fc35f42b3e49b7669568c44
vwkd <33468089+vwkd@users.noreply.github.com>
2022-08-31 21:36:07 +0400

    parser: fill in reference

--- src/parser/reference.ts
+++ src/parser/reference.ts
@@ -12,27 +12,90 @@ import tagsParser from "./tag.ts";
 
 /*
 Reference
+    ReferenceValue
+    ReferenceValueTagged
+*/
+const referenceParser = choice([
+  referenceValueParser,
+  referenceValueTaggedParser
+]);
+
+/*
+ReferenceValueTagged
+    Tags ws ReferenceValue
+*/
+const referenceValueTaggedParser = coroutine( function* () {
+  const tags = yield tagsParser,
+  yield whitespaceParser;
+  const reference = yield referenceValueParser;
+  
+  return {
+    ...reference
+    // note: overwrites empty tags property from referenceValueParser!
+    tags
+  }
+});
+
+/*
+ReferenceValue
     ReferenceDirect
     ReferenceMeaning
     ReferenceIdentical
 */
+const referenceValueParser = choice([
+  referenceDirectParser,
+  referenceMeaningParser,
+  referenceIdenticalParser
+]).map(o => {
+  o.tags = [];
+  return o;
+});
 
 /*
 ReferenceDirect
     "s." ws Key
-    Tags ws "s." ws Key
 */
+const referenceDirectParser = coroutine( function* () {
+  yield str("s."),
+  yield whitespaceParser;
+  const key = yield keyParser;
+  
+  return {
+    kind: "direct",
+    key,
+  }
+});
 
 /*
 ReferenceMeaning
-    "s." ws Bed. ws Key
-    Tags ws "s." ws Bed. ws Key
+    "s." ws "Bed." ws Key
 */
+const referenceMeaningParser = coroutine( function* () {
+  yield str("s."),
+  yield whitespaceParser;
+  yield str("Bed."),
+  yield whitespaceParser;
+  const key = yield keyParser;
+  
+  return {
+    kind: "meaning",
+    key,
+  }
+});
 
 /*
 ReferenceIdentical
     "id." ws Key
-    Tags ws "id." ws Key
 */
+const referenceIdenticalParser = coroutine( function* () {
+  yield str("id."),
+  yield whitespaceParser;
+  const key = yield keyParser;
+  
+  return {
+    kind: "identical",
+    key,
+  }
+});
 
 export default referenceParser;
