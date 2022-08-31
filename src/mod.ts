2a247e90e429212fe97cb337ca4a7d17caff9041
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-01 00:30:09 +0400

    use longer test string

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,17 @@
 import parser from "./parser/mod.ts"
 
 const input = `ადგილ umstellen, translozieren
-ადვილ erleichtern`;
+ადვილ erleichtern
+ავ böse sein
+ავად s. ავადმყოფ
+ავყია id. ავყიავ
+არ² s. ყოფნ
+ასპარეზ {va.} wettkämpfen
+ახლ³ s. ხლ³
+ბალთ {kach., psch.} zerstückeln
+ბორძიკ 1. stolpern 2. stammeln
+ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
+`;
 
 parser.fork(input,
   (error, _) => console.error(error),
