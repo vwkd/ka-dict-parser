0077191f8cd517ec202740bdffd352ed82b288ac
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-01 13:24:17 +0400

    test: simplify test string more

--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,6 @@
 import parser from "./parser/mod.ts"
 
-const input = `ბორძიკ 1. stolpern 2. stammeln
-ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
-გებულ 1. verstehen 2. vernehmen 3. gewinnen
-გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben
-`;
+const input = `ბორძიკ 1. stolpern 2. stammeln`;
 
 parser.fork(input,
   (error, _) => console.error("Error:", error),
