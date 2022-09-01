5ee72b5fd1b125e56f65c5839d3838939735062d
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-01 15:25:36 +0400

    test: use full test string again

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,15 @@
 import parser from "./parser/mod.ts"
 
-const input = `ბორძიკ 1. stolpern 2. stammeln
+const input = `ადგილ umstellen, translozieren
+ადვილ erleichtern
+ავ böse sein
+ავად s. ავადმყოფ
+ავყია id. ავყიავ
+არ² s. ყოფნ
+ასპარეზ {va.} wettkämpfen
+ახლ³ s. ხლ³
+ბალთ {kach., psch.} zerstückeln
+ბორძიკ 1. stolpern 2. stammeln
 ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
 გებულ 1. verstehen 2. vernehmen 3. gewinnen
 გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben
