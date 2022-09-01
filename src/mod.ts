5dcdd90ae86674bb01da61dbc0bb66b7ac36626d
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-02 02:00:46 +0400

    test: remove trailing new line from test string

--- src/mod.ts
+++ src/mod.ts
@@ -12,8 +12,7 @@ const input = `ადგილ umstellen, translozieren
 ბორძიკ 1. stolpern 2. stammeln
 ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
 გებულ 1. verstehen 2. vernehmen 3. gewinnen
-გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben
-`;
+გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben`;
 
 parser.fork(input,
   (error, _) => console.error("Error:", error),
