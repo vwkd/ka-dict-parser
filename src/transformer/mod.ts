43a5becf7b4eb26a532a906bb4ba93aa31539516
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-05 17:13:11 +0400

    transformer: add addId and addReferenceId

---
+++ src/transformer/mod.ts
@@ -0,0 +1,48 @@
+import { equal } from "../deps.ts";
+import type { EntryType } from "../types.ts";
+
+export default function transform(entries: EntryType[]) {
+  const p = pipe(
+    addId,
+    addReferenceId,
+  );
+  
+  return p(entries);
+}
+
+/* Add entry.id
+ * uses line number as id (position in entries array)
+*/
+function addId(entries: EntryType[]) {
+  return entries.map((e, id) => ({ id, ...e,}));
+}
+
+/* Add entry.reference?.id
+ * beware: assumes entries have unique source, no duplicates!
+*/
+function addReferenceId(entries: EntryType[]) {
+  return entries.map(e => {
+    const reference = e.reference
+    
+    if (reference) {
+    
+      const source = reference.source;
+      
+      const eRef = entries.find(f => equal(f.source, source));
+      
+      if (!eRef) {
+        throw new Error(`Couldn't find referenced entry '${Object.values(source).join("^")}' at entry '${Object.values(e.source).join("^")}'.`);
+      }
+      
+      const id = eRef.id;
+      e.reference = {
+        id,
+        ...e.reference,
+      };
+    }
+    
+    return e;
+  });
+}
+
+const pipe = (...fns) => (x) => fns.reduce((res, fn) => fn(res), x);
