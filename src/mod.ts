b3233dc0f824360edea51c1bbe0b983126dccc77
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-05 17:28:34 +0400

    mod: add transformer, add input output

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,7 @@
 import parser from "./parser/mod.ts"
+import transformer from "./transformer/mod.ts";
+
+// const input = await Deno.readTextFile("");
 
 const input = `ადგილ umstellen, translozieren
 ადვილ erleichtern
@@ -14,7 +17,17 @@ const input = `ადგილ umstellen, translozieren
 გებულ 1. verstehen 2. vernehmen 3. gewinnen
 გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben`;
 
-parser.fork(input,
-  (error, _) => console.error("Error:", error),
-  (result, _) => console.log("Success:", result)
+const result = parser.fork(input,
+  (error, _) => {
+    console.error("Parse error:", error);
+    throw error;
+  },
+  (result, _) => {
+    console.log("Parse success:", result.slice(0,5));
+    const result2 = transformer(result);
+    console.log("Transform success:", result2.slice(0, 5));
+    return result2;
+  }
 );
+
+// await Deno.writeTextFile(result, "out/output.json");
