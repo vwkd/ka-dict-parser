2fa37392ad6a7a7b404e3133bb9dcce535582ecf
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-06 12:04:33 +0400

    Revert "mod: use shorter test string with matching reference entries"
    This reverts commit 4a54bcd5637c56fc078fb00bc26b38b5aa9ee45a.

--- src/mod.ts
+++ src/mod.ts
@@ -6,8 +6,16 @@ import transformer from "./transformer/mod.ts";
 const input = `ადგილ umstellen, translozieren
 ადვილ erleichtern
 ავ böse sein
-ავდარ s. ავდრ
-ავდრ schlechtes Wetter sein`;
+ავად s. ავადმყოფ
+ავყია id. ავყიავ
+არ² s. ყოფნ
+ასპარეზ {va.} wettkämpfen
+ახლ³ s. ხლ³
+ბალთ {kach., psch.} zerstückeln
+ბორძიკ 1. stolpern 2. stammeln
+ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
+გებულ 1. verstehen 2. vernehmen 3. gewinnen
+გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben`;
 
 const result = parser.fork(input,
   (error, _) => {
