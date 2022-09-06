2b38437ce0679dc8d8794885dbd30d37ad5f18e9
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-06 14:03:47 +0400

    mod: fetch test file directly

--- src/mod.ts
+++ src/mod.ts
@@ -1,25 +1,8 @@
 import parser from "./parser/mod.ts"
 import transformer from "./transformer/mod.ts";
 
-// const input = await Deno.readTextFile("");
-
-const input = `ადგილ umstellen, translozieren
-ადვილ erleichtern
-ავ böse sein
-ასპარეზ {va.} wettkämpfen
-ბალთ {kach., psch.} zerstückeln
-ბორძიკ 1. stolpern 2. stammeln
-ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
-გებულ 1. verstehen 2. vernehmen 3. gewinnen
-გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben
-ავდარ s. ავდრ
-ავდრ schlechtes Wetter sein
-ასკინკილა id. ასკინკილავ
-ასკინკილავ auf einem Bein hüpfen
-ენამზე Bed. s. ენამჭევრ
-ენამჭევრ redegewandt sein
-ლაქავ beflecken, beklecksen
-ლაქიან Bed. s. ლაქავ`;
+const res = await fetch("https://raw.githubusercontent.com/vwkd/ka-dict-verbs/main/vz/vz.txt");
+const input = await res.text();
 
 const result = parser.fork(input,
   (error, _) => {
@@ -34,4 +17,4 @@ const result = parser.fork(input,
   }
 );
 
-await Deno.writeTextFile("out/vz.json", JSON.stringify(result));
+await Deno.writeTextFile("vz.json", JSON.stringify(result));
