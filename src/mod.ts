742b01164c21240b9bbd4d75f3ab69f1bad61a84
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-06 12:25:59 +0400

    SQUASH mod: use shorter test string with matching reference entries RENAME use better test string, add matching reference entries

--- src/mod.ts
+++ src/mod.ts
@@ -6,16 +6,19 @@ import transformer from "./transformer/mod.ts";
 const input = `ადგილ umstellen, translozieren
 ადვილ erleichtern
 ავ böse sein
-ავად s. ავადმყოფ
-ავყია id. ავყიავ
-არ² s. ყოფნ
 ასპარეზ {va.} wettkämpfen
-ახლ³ s. ხლ³
 ბალთ {kach., psch.} zerstückeln
 ბორძიკ 1. stolpern 2. stammeln
 ბუ² 1. {kach.} aufschütteln 2. {kach.} anschwellen
 გებულ 1. verstehen 2. vernehmen 3. gewinnen
-გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben`;
+გონ 1. vernehmen, hören 2. gedenken, sich erinnern 3. ersinnen, erfinden 4. vortäuschen 5. meinen, glauben
+ავდარ s. ავდრ
+ავდრ schlechtes Wetter sein
+ასკინკილა id. ასკინკილავ
+ასკინკილავ auf einem Bein hüpfen
+ენამზე Bed. s. ენამჭევრ
+ენამჭევრ redegewandt sein
+ვაზირ {va.} Bed. s. ვეზირ`;
 
 const result = parser.fork(input,
   (error, _) => {
