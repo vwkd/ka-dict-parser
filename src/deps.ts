3a3285773fce05adfc519ad10ec8d42d48b2643d
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-07 00:58:09 +0400

    parse: don't add id anymore, use transformer instead

--- src/deps.ts
+++ src/deps.ts
@@ -9,8 +9,5 @@ export {
   many1,
   startOfInput,
   endOfInput,
-  withData,
-  setData,
-  getData,
 } from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
