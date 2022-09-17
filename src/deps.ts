9463d451b426bf41e753a384bffb074346dce95e
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-17 22:05:09 +0400

    parse: add meaning to references using "(Pkt. X)"

--- src/deps.ts
+++ src/deps.ts
@@ -9,5 +9,6 @@ export {
   many1,
   startOfInput,
   endOfInput,
+  digits,
 } from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
