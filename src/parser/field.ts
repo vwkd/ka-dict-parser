eb4888bad8cebaa88e0dc1c2232bf3b4a826926a
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-18 10:22:15 +0400

    parse: add category, rename field

---
+++ src/parser/field.ts
@@ -0,0 +1,116 @@
+import {
+  coroutine,
+  char,
+  many,
+  possibly,
+  str,
+} from "../deps.ts";
+
+import { whitespaceParser } from "./chars.ts";
+import { wordsParser } from "./word.ts";
+import tagsWhitespaceParser from "./tags.ts";
+
+/*
+CommaWhitespaceWords
+    "," ws Words
+*/
+const commaWhitespaceWordsParser = coroutine( function* () {
+  yield char(",");
+  yield whitespaceParser;
+  const words = yield wordsParser;
+  
+  return words;
+});
+
+/*
+CategoryList
+    Words CommaWhitespaceWords*
+*/
+const categoryListParser = coroutine( function* () {
+  const category = yield wordsParser;
+  const categoryList = yield many( commaWhitespaceWordsParser);
+  
+  return [
+    category,
+    ...categoryList
+  ];
+});
+
+/*
+Category
+    "(" CategoryList ")"
+*/
+const categoryParser = coroutine( function* () {
+  yield char("(");
+  const categoryList = yield categoryListParser;
+  yield char(")");
+  
+  return categoryList;
+});
+
+/*
+WhitespaceCategory
+    ws Category
+*/
+const whitespaceCategoryParser = coroutine( function* () {
+  yield whitespaceParser;
+  const category = yield categoryParser;
+  
+  return category;
+});
+
+/*
+Element
+    Words WhitespaceCategory?
+*/
+const elementParser = coroutine( function* () {
+  const value = yield wordsParser;
+  const category = (yield possibly( whitespaceCategoryParser)) ?? [];
+
+  return {
+    value,
+    category,
+  };
+});
+
+/*
+CommaWhitespaceElement
+    "," ws Element
+*/
+const commaWhitespaceElementParser = coroutine( function* () {
+  yield char(",");
+  yield whitespaceParser;
+  const element = yield elementParser;
+  
+  return element;
+});
+
+/*
+Elements
+    Element CommaWhitespaceElement*
+*/
+const elementsParser = coroutine( function* () {
+  const element = yield elementParser;
+  const elementList = yield many( commaWhitespaceElementParser);
+
+  return [
+    element,
+    ...elementList,
+  ];
+});
+
+/*
+Field
+    TagsWhitespace? Elements
+*/
+const fieldParser = coroutine( function* () {
+  const tags = (yield possibly( tagsWhitespaceParser)) ?? [];
+  const value = yield elementsParser;
+
+  return {
+    value,
+    tags,
+  };
+});
+
+export default fieldParser;
