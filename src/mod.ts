a5162c150afd7d5a5bae498497b995114c850468
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-28 20:00:00 +0400

    mod: improve log order

--- src/mod.ts
+++ src/mod.ts
@@ -15,13 +15,6 @@ const parseResult = parser.fork(input, handleError, handleSuccess);
 function handleError(error, parsingState) {
   // TODO: first error is on newline before problematic line due to trying to bail out into endOfInput
   
-  console.error("Parse error:", error);
-  console.error("Parse target:", parsingState.data);
-  
-  // if breaks within first line return only part of object without array
-  const resultBefore = Array.isArray(parsingState.result) ? parsingState.result : [];
-  console.log("Parse result:", resultBefore);
-  
   const indexFailure = parsingState.index;
   
   // beware: doesn't work when indexFailure is exactly on newline
@@ -33,6 +26,13 @@ function handleError(error, parsingState) {
   // todo: for some reason doesn't need `indexNewlineBeforeBounded + 1`
   const line = inputRest.slice(indexNewlineBeforeBounded, indexNewlineAfterBounded);
   console.log("Can't parse line:", decoder.decode(line));
+  
+  console.error("Parse error:", error);
+  console.error("Parse target:", parsingState.data);
+  
+  // if breaks within first line return only part of object without array
+  const resultBefore = Array.isArray(parsingState.result) ? parsingState.result : [];
+  console.log("Parse result:", resultBefore);
   
   // continue with next line
   inputRest = inputRest.slice(indexNewlineAfterBounded + 1);
