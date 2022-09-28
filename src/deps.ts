776ff2ae6b3a4a8e86f408435d900652220ecac4
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-28 13:21:48 +0400

    FIXUP ?? add arcsecond deps

--- src/deps.ts
+++ src/deps.ts
@@ -10,5 +10,5 @@ export {
   startOfInput,
   endOfInput,
   digits,
-} from "https://cdn.skypack.dev/arcsecond";
+} from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
