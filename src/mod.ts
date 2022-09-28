0ebb9e4d023f2a772d231fbde06257ed21ce4eae
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-28 13:03:07 +0400

    mod: polish, separate handler functions

--- src/mod.ts
+++ src/mod.ts
@@ -4,19 +4,22 @@ import transformer from "./transformer/mod.ts";
 const res = await fetch("https://raw.githubusercontent.com/vwkd/ka-dict-verbs/main/vz/vz.txt");
 const input = await res.text();
 
-const parseResult = parser.fork(input,
-  (error, parsingState) => {
-    console.error("Parse error:", error);
-    console.error("Parse target:", parsingState.data);
-    console.log(parsingState.result.at(-1));
-    // throw error;
-    return parsingState.result
-  },
-  (result, _) => {
-    console.log("Parse success");
-    return result;
-  }
-);
+const parseResult = parser.fork(input, handleError, handleSuccess);
+
+function handleError(error, parsingState) {
+  console.error("Parse error:", error);
+  console.error("Parse target:", parsingState.data);
+  console.log(parsingState.result.at(-1));
+
+  return parsingState.result;
+}
+
+function handleSuccess(result, _) {
+  console.log("Parse success");
+  console.log(result.at(-1));
+  
+  return result;
+}
 
 const result = transformer(parseResult);
 console.log("Transform success");
