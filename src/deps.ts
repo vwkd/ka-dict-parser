acdfc47d2e28484aaa5806f5494d40e753ae1203
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-30 01:25:34 +0400

    parse: simplify definition list, use sepBy, use internal state

--- src/deps.ts
+++ src/deps.ts
@@ -13,5 +13,8 @@ export {
   digits,
   anyCharExcept,
   skip,
+  getData,
+  setData,
+  withData,
 } from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
