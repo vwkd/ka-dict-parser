851f54b3d719ddbc249678ef26ef85eb2ee9657b
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-29 20:42:31 +0400

    utils: factor out bytes to code point conversion

--- src/mod.ts
+++ src/mod.ts
@@ -1,21 +1,10 @@
 import parser from "./parser/mod.ts"
 import transformer from "./transformer/mod.ts";
-
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
-
-const res = await fetch("https://raw.githubusercontent.com/vwkd/ka-dict-verbs/main/vz/vz.txt");
-const inputStr = await res.text();
-const input = encoder.encode(inputStr);
+import { ByteCodePointConverter } from "./utils.ts";
+import { inputObj } from "./deps.ts";
 
 function handleError(error, parsingState) {
-  console.log("Parse failure");
-  
-  const indexFailure = parsingState.index;
-  
-  const inputBefore = input.slice(0, indexFailure);
-  const indexFailureCodePoint = decoder.decode(inputBefore).length;
-  console.error(error.replace(/\d+/, indexFailureCodePoint));
+  console.error("Parse failure:", error.replace(/\d+/, inputObj.getPointIndex(parsingState.index)));
   
   console.error("Parse target:", parsingState.data);
 
@@ -28,7 +17,7 @@ function handleSuccess(result, _) {
   return result;
 }
 
-const parseResult = parser.fork(input, handleError, handleSuccess);
+const parseResult = parser.fork(inputObj.bytes, handleError, handleSuccess);
 
 const result = transformer(parseResult);
 console.log("Transform success");
