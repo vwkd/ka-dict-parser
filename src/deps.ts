25d47b304d132addcbbc4787b525313363cede65
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-30 01:10:19 +0400

    parse: use sepBy instead of many in word, fix sepBy1

--- src/deps.ts
+++ src/deps.ts
@@ -6,7 +6,6 @@ export {
   char,
   possibly,
   many,
-  sepBy1,
   many1,
   between,
   startOfInput,
@@ -18,7 +17,20 @@ export {
   setData,
   withData,
 } from "https://cdn.skypack.dev/arcsecond@v4.1.0";
+import {
+  sequenceOf,
+  many,
+} from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+
+// fixed version, see https://github.com/francisrstokes/arcsecond/issues/98
+export const sepBy1 = separator => value => sequenceOf([
+  value,
+  many(sequenceOf([
+    separator,
+    value
+  ]).map(a => a[1])),
+]).map(([e, r]) => [e, ...r]);
 
 import { ByteCodePointConverter } from "./utils.ts";
 const res = await fetch("https://raw.githubusercontent.com/vwkd/ka-dict-verbs/main/src/vz.txt");
