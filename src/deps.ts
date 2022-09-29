29fe7a6034fae5f42bdd4a0181b024ef47f21172
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-29 18:25:07 +0400

    parse: recover error in parser instead of mod

--- src/deps.ts
+++ src/deps.ts
@@ -11,5 +11,7 @@ export {
   startOfInput,
   endOfInput,
   digits,
+  anyCharExcept,
+  skip,
 } from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
