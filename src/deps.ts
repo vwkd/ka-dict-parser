d49b7e0c60cce85c872f9373600f1af3c3ccc197
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-29 20:42:31 +0400

    utils: factor out bytes to code point conversion

--- src/deps.ts
+++ src/deps.ts
@@ -19,3 +19,7 @@ export {
   withData,
 } from "https://cdn.skypack.dev/arcsecond@v4.1.0";
 export { equal } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+
+const res = await fetch("https://raw.githubusercontent.com/vwkd/ka-dict-verbs/main/src/vz.txt");
+const input = await res.text();
+export const inputObj = ByteCodePointConverter(input);
