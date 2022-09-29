1462dc5f008d8bc349e4b7e6d96e9710b569331b
vwkd <33468089+vwkd@users.noreply.github.com>
2022-09-29 22:45:49 +0400

    parse: error log line number, line text and line index

--- src/utils.ts
+++ src/utils.ts
@@ -21,10 +21,40 @@ export function ByteCodePointConverter(input) {
     return byteIndex;
   }
   
+  // note: first line number is one, not zero
+  function getLineNumber(pointIndex) {
+    return (points.slice(0, pointIndex).match(/\n/g)?.length ?? 0) + 1;
+  }
+  
+  function getLineText(pointIndex) {
+    // todo: what if error is on newline? then `indexNewlineBefore == indexNewlineAfter == indexFailure`
+    
+    // beware: if error in first line, then indexNewlineBefore is -1, don't change to 0, because `indexNewlineBefore + 1` luckily happens to then compute to 0 already
+    const indexNewlineBefore = points.lastIndexOf("\n", pointIndex);
+    // beware: if error in last line, then indexNewlineAfter is -1, change to Infinity
+    const indexNewlineAfterMaybe = points.indexOf("\n", pointIndex);
+    const indexNewlineAfter = indexNewlineAfterMaybe == -1 ? Infinity : indexNewlineAfterMaybe;
+    
+    return points.slice(indexNewlineBefore + 1, indexNewlineAfter);
+  }
+  
+  // note: first line index is one, not zero
+  function getLineIndex(pointIndex) {
+    // todo: what if error is on newline? then `indexNewlineBefore == indexNewlineAfter == indexFailure`
+    
+    // beware: if error in first line, then indexNewlineBefore is -1, don't change to 0, because `- -1` luckily happens to then compute to 1 already
+    const indexNewlineBefore = points.lastIndexOf("\n", pointIndex);
+    
+    return pointIndex - indexNewlineBefore;
+  }
+  
   return {
     points,
     bytes,
     getPointIndex,
     getByteIndex,
+    getLineNumber,
+    getLineText,
+    getLineIndex,
   }
 }
