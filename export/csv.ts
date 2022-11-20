0a6a76fbacdf2d46ec9588119b2e9e4e91330677
vwkd <33468089+vwkd@users.noreply.github.com>
2022-11-20 20:07:56 +0200

    export: add sources

--- export/csv.ts
+++ export/csv.ts
@@ -1,17 +1,33 @@
 import { stringify } from "$std/encoding/csv.ts";
 
+import sources from "../sources.json" assert { type: "json" } ;
 import tags from "../tags.json" assert { type: "json" } ;
 import categories from "../categories.json" assert { type: "json" } ;
 
 const exports = {
+    "sources": sources,
     "tags": tags,
     "categories": categories,
 }
 
 for (const [name, data] of Object.entries(exports)) {
-    console.log(`Exporting ${name}.csv`, name);
+    console.log(`Exporting ${name}.csv`);
 
-    const columns = Object.getOwnPropertyNames(data[0]);
+    const propertyNames = new Set();
+    
+    // beware: order not necessarily like object, e.g.
+    // optional property not at end
+    // multiple optional properties depending on order of entries
+    // etc.
+    for (const entry of data) {
+        const propNames = Object.getOwnPropertyNames(entry);
+        
+        for (const propName of propNames) {
+            propertyNames.add(propName);
+        }
+    }
+
+    const columns = Array.from(propertyNames);
 
     await Deno.writeTextFile(`${name}.csv`, stringify(data, { columns }));
 }
---
+++ export/sources.ts
@@ -0,0 +1,7 @@
+import vz from "../vz.json" assert { type: "json" } ;
+
+console.log("Extracting sources into sources.json");
+
+const sources = vz.map(({ source: { value, meaning }}, id) => ({ id: id + 1, value, meaning }));
+
+await Deno.writeTextFile("sources.json", JSON.stringify(sources, null, 2));
